a1a0acb68828d62ef80c763f24d62e3c6143b2fe
ivankadikova-droid <ivankadikova62@gmail.com>
2026-08-06 10:38:28 +0300

    Add files

---
+++ build.js
@@ -0,0 +1,4 @@
+console.log("Starting test build for Site Tools deployment logs...");
+console.log("Installing dependencies completed successfully.");
+console.log("Build completed successfully: DES-15862 deployment log test.");
+process.exit(0);
