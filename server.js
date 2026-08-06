277087a06a992f7538add818a56e0f63afbb5534
ivankadikova-droid <ivankadikova62@gmail.com>
2026-08-06 09:30:39 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const port = process.env.PORT || 3000;
+
+http
+  .createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("This app should not start because the build fails first.\n");
+  })
+  .listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
