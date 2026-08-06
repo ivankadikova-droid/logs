5eaac3933debd9814d5510ef0e6051d2a3110e5e
ivankadikova-droid <ivankadikova62@gmail.com>
2026-08-06 10:29:25 +0300

    Delete fail-build.js

--- fail-build.js
+++
@@ -1,5 +0,0 @@
-console.log("Starting test build for Site Tools deployment logs...");
-console.log("Installing dependencies completed successfully.");
-console.error("Intentional build failure: DES-15862 deployment log test.");
-console.error("This archive is expected to fail during npm run build.");
-process.exit(1);
